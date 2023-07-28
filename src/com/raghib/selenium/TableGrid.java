fd16152d62d061f2103dc53c799286fc1d1e0ff1
usmaniraghib <javaprogrammers786@gmail.com>
2023-07-28 12:42:42 +0530

    Warning Removed

--- src/com/raghib/selenium/TableGrid.java
+++ src/com/raghib/selenium/TableGrid.java
@@ -14,7 +14,7 @@ public class TableGrid {
 				"http://www.cricbuzz.com/live-cricket-scorecard/18970/pak-vs-sl-2nd-t20i-pakistan-v-sri-lanka-in-uae-2017");
 
 		WebElement table = driver.findElement(By.cssSelector("div[class='cb-col cb-col-100 cb-ltst-wgt-hdr']"));
-		int rowcount = table.findElements(By.cssSelector("cb-col cb-col-100 cb-scrd-itms")).size();
+		//int rowcount = table.findElements(By.cssSelector("cb-col cb-col-100 cb-scrd-itms")).size();
 		int count = table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)"))
 				.size();
 
